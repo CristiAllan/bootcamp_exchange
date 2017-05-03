c2ab8b1903296260e4d657e9ddafbc56354cbd31
CristiAllan <cristiallan@gmail.com>
2017-05-03 12:09:46 -0300

    All view setups done

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Exchange Currency Process", :type => :feature do 
 	it 'exchange value' do
 		visit '/'
-		within("exchange_form") do 
+		within("#exchange_form") do 
 			select('EUR', from: 'currency')
 			select('USD', from: 'currency_destination')
 			fill_in 'quantity', with: '10'
@@ -11,6 +11,6 @@ RSpec.describe "Exchange Currency Process", :type => :feature do
 		click_button 'CONVERTER'
 
 		save_and_open_page
-		expect(page).to have_content(value)
+		expect(page).to have_content("value")
 	end
 end
