3c395e2f59aab99c4a8994aa692b6d2be4f358b7
CristiAllan <cristiallan@gmail.com>
2017-05-08 23:21:32 -0300

    Add translations english and portuguese

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap', '~> 4.0.0.alpha3'
 gem 'rest-client'
 gem 'font-awesome-sass'
+gem 'rails-i18n'
+
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- config/application.rb
+++ config/application.rb
@@ -18,10 +18,13 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module BootcampExchange
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.available_locales = [:en, :'pt-br']
+		config.i18n.default_locale = :'pt-br'
   end
 end
